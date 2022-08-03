99ce58121031d7b919bda93f7a3b706ff9573661
FlutLab.io <>
2022-08-03 10:55:48 +0000

    Autosave

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -54,12 +54,12 @@ class _MyHomePageState extends State<SplashScreen> {
                 ),
               ),
             ),
-            decoration: BoxDecoration(shape: BoxShape.rectangle, color: Colors.white, boxShadow: [
+            decoration: BoxDecoration(shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(20)), color: Colors.white, boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                blurRadius: 2.0,
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 4.0,
                 offset: Offset(5.0, 3.0),
-                spreadRadius: 10.0,
+                spreadRadius: 1.0,
               )
             ]),
           ),
